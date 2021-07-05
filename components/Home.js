ad22e850592c23a88db65f8630f33e6648c96caf
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2021-07-05 21:09:25 +0530

    added styels in components

---
+++ components/Home.js
@@ -0,0 +1,14 @@
+import React  from "react";
+import { Text } from "react-native";
+
+
+const Home = () => {
+    return (
+
+        <Text>This is home</Text>
+    );
+}
+
+export default Home;
+
+
